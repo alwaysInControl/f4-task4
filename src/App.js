19cffe8e68a8305aefb84603ddbd0043856d0eed
alwaysInControl <127772513+alwaysInControl@users.noreply.github.com>
2023-05-27 13:49:23 +0600

    register form

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './registerForm.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class RegisterForm extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            fullName: '',
+            phone: '',
+            email: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+    }
+
+    handleChange(event) {
+        this.setState({ [event.target.name]: event.target.value });
+    }
+
+    handleSubmit(event) {
+        alert(`Full name: ${this.state.fullName}\nPhone number: ${this.state.phone}\nEmail: ${this.state.email}`)
+        event.preventDefault()
+    }
+
+    render() {
+        return(
+            <form onSubmit={this.handleSubmit} className='formContainer'>
+                <label>
+                    Name and surname:
+                    <input type="text" name='fullName' value={this.state.value} onChange={this.handleChange}
+                           className='input'/>
+                </label>
+                <label>
+                    Phone:
+                    <input type="text" name='phone' value={this.state.value} onChange={this.handleChange}
+                           className='input'/>
+                </label>
+                <label>
+                    Email:
+                    <input type="text" name='email' value={this.state.value} onChange={this.handleChange}
+                           className='input'/>
+                </label>
+                    <input type="submit" value='Submit my info' className='submitButton'/>
+            </form>
+        )
+    }
 }
 
-export default App;
+export default RegisterForm;
